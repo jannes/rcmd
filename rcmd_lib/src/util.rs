0bc2121929808f68ea8135f9ea907c846859c882
Jannes Timm <jannes.timm@bloomreach.com>
2021-10-07 16:06:39 +0200

    Utils for managing process

--- rcmd_lib/src/util.rs
+++ rcmd_lib/src/util.rs
@@ -1,23 +1,60 @@
-use tokio::{io::AsyncRead, process::ChildStderr};
+use std::process::ExitStatus;
 
-async fn consume_outstream(outstream: ChildStderr) -> String {
-    todo!()
-    // let mut buffer = [0; 512];
-    // let mut text = Vec::new();
-    // 'l: loop {
-    //     tokio::select! {
-    //         result = outstream.read(&mut buffer) => {
-    //             let bytes_read = result?;
-    //             if bytes_read == 0 {
-    //                 break 'l;
-    //             }
-    //             text.extend_from_slice(&buffer[0..bytes_read]);
-    //         }
-    //         _ = sleep(Duration::from_millis(5)) => {
-    //             break 'l;
-    //         }
-    //     };
-    // }
-    // let text = String::from_utf8(text).unwrap_or_else(|_| "NON-UTF8".to_string());
-    // text
+use tokio::{
+    io::{self, AsyncBufReadExt, AsyncRead, BufReader},
+    process::Child,
+    sync::mpsc,
+};
+
+pub async fn manage_process(
+    mut process: Child,
+    stdout_channel: mpsc::Sender<String>,
+    stderr_channel: mpsc::Sender<String>,
+    exit_channel: mpsc::Sender<io::Result<ExitStatus>>,
+    mut kill_channel: mpsc::Receiver<()>,
+) {
+    let stdout = process.stdout.take().unwrap();
+    let stderr = process.stderr.take().unwrap();
+
+    // continously read from stdout/stderr in background
+    let _ = tokio::spawn(async { read_to_end(stdout, stdout_channel) });
+    let _ = tokio::spawn(async {
+        read_to_end(stderr, stderr_channel).await;
+    });
+
+    // either wait for proces to finish or receive a terminate command
+    tokio::select! {
+        _ = process.wait() => { }
+        _ = kill_channel.recv() => {
+            if let Err(e) = process.kill().await {
+                todo!()
+            }
+            kill_channel.close()
+        }
+    }
+
+    // wait for process to finish, send exit status / error on exit channel
+    let res = exit_channel.send(process.wait().await).await;
+    if let Err(send_error) = res {
+        todo!()
+    }
+}
+
+async fn read_to_end<A: AsyncRead + std::marker::Unpin>(
+    stream: A,
+    out_channel: mpsc::Sender<String>,
+) {
+    let mut reader = BufReader::new(stream);
+    loop {
+        let mut buf = String::new();
+        match reader.read_line(&mut buf).await {
+            Ok(n) if n == 0 => break,
+            Ok(_) => {}
+            Err(_) => todo!(),
+        }
+        match out_channel.send(buf).await {
+            Ok(_) => todo!(),
+            Err(_) => todo!(),
+        }
+    }
 }
